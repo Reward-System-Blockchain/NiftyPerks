617aab6bd2bc896770791eb09f5527d8f1f547b4
Harsh Gupta <harsh.gupta2211@gmail.com>
2023-03-25 23:48:25 +0530

    added profile page

--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 import { Provider } from "react-redux";
-import Store from './app/Store.js';
-import { Toaster } from 'react-hot-toast';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Root from './components/routes/root';
+import Store from "./app/Store.js";
+import { Toaster } from "react-hot-toast";
+import routes from "./routes";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element:<Provider store={Store}>
-    <Toaster position='top-center' reverseOrder={false} />
-      <App/>
-    </Provider>,
-    
-  },
-  {
-    path: "/user",
-    element: <Root />,
-  },
-  
-]);
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-     <RouterProvider router={router} />
-    
+    <Provider store={Store}>
+      <BrowserRouter>
+        <Toaster position="top-center" reverseOrder={false} />
+        <Routes>
+          {routes.map((route) => (
+            <Route
+              key={route.path}
+              path={route.path}
+              element={<route.component />}
+            />
+          ))}
+        </Routes>
+        {/* <App /> */}
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
-)
+);
---
+++ src/routes.js
@@ -0,0 +1,8 @@
+import Profile from "./components/profile";
+import App from "./App";
+const routes = [
+    { path: '/', component: App},
+    { path: '/profile', component: Profile },
+];
+
+export default routes;
