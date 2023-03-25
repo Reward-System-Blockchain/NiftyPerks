41621e71e02c8eea97552d6652921eddf5bf85ac
Prashant Prithyani <prithyani2002@gmail.com>
2023-03-26 00:36:10 +0530

    '.'

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import Navbar from "./Navbar.jsx";
+import { Card1 } from "./utils/Card1.jsx";
+import { Grid } from "@nextui-org/react";
 
 const Profile = () => {
   return (
-    <div>
+    <div className="h-screen flex flex-col">
+    
       <Navbar />
       <h1>Profile</h1>
       <p>This is a profile page.</p>
+    
+    <Grid.Container gap={2} justify="center">
+    
+    <Grid xs={12} sm={4}>
+      <Card1 />
+    </Grid>
+    
+   
+  </Grid.Container>
     </div>
+    
   );
 };
 
