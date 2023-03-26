b8ff811ffedf5a00980c846bc373dccca136f7f6
Harsh Gupta <harsh.gupta2211@gmail.com>
2023-03-26 13:34:14 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    middleware: {
+      // If no routes match, send the index.html file
+      // This allows client-side routing to work
+      after: (req, res) => {
+        res.statusCode = 200;
+        res.end(fs.readFileSync('dist/index.html'));
+      },
+    },
+  },
 })
