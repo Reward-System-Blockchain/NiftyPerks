3db74904ff1918e56541763ae1a4d5d479d10645
Harsh Gupta <harsh.gupta2211@gmail.com>
2023-03-26 16:23:39 +0530

    added push chat support

--- src/App.jsx
+++ src/App.jsx
@@ -19,60 +19,49 @@ import {
   footerAPI,
 } from "./data/data.js";
 
-import {
-  EthereumClient,
-  w3mConnectors,
-  w3mProvider,
-} from "@web3modal/ethereum";
-import { Web3Modal } from "@web3modal/react";
-import { configureChains, createClient, WagmiConfig } from "wagmi";
-import { polygonMumbai, goerli, mainnet, polygon } from "wagmi/chains";
-
-const chains = [polygonMumbai, goerli, mainnet, polygon];
-const projectId = "f32c7097c07c8c1552f46619147dff35";
-
-const { provider } = configureChains(chains, [w3mProvider({ projectId })]);
-const wagmiClient = createClient({
-  autoConnect: true,
-  connectors: w3mConnectors({ projectId, version: 1, chains }),
-  provider,
-});
-const ethereumClient = new EthereumClient(wagmiClient, chains);
-
+import { useState, useEffect } from "react";
 const App = () => {
+  const [isWalletConnected, setIsWalletConnected] = useState(false);
+  const [walletAddress, setWalletAddress] = useState(null);
+
+  async function getAccountAddress() {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        alert("Please Install MetaMask");
+        return;
+      }
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setIsWalletConnected(true);
+      setWalletAddress(accounts[0]);
+      localStorage.setItem("walletAddress", accounts[0]);
+      // setAddress(accounts[0]);
+      console.log(localStorage.getItem("walletAddress"));
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getAccountAddress();
+  }, []);
+
   return (
     <>
       {" "}
-      <WagmiConfig client={wagmiClient}>
-        <Navbar />
-        <Cart />
-        <main className="flex flex-col gap-16 relative">
-          <Hero heroapi={heroapi} />
-          <Sales1 endpoint={popularsales} ifExists />
-          <FlexContent endpoint={highlight} ifExists />
-          <Sales endpoint={toprateslaes} />
-          <FlexContent endpoint={sneaker} />
-          <Stories story={story} />
-        </main>
-        <Footer footerAPI={footerAPI} />
-      </WagmiConfig>
-      <Web3Modal
-        projectId={projectId}
-        ethereumClient={ethereumClient}
-        themeVariables={
-          {
-            // '--w3m-font-family': 'ui-sans-serif,system-ui',
-            // "--w3m-accent-color": "#334155",
-          }
-        }
-        themeMode="light"
-        enableNetworkView="true"
-        chainImages={{
-          5: "https://assets-global.website-files.com/6364e65656ab107e465325d2/637aee14aa9d9f521437ec16_hYC2y965v3QD7fEoVvutzGbJzVGLSOk6RZPwEQWcA_E.jpeg",
-          80001:
-            "https://assets-global.website-files.com/6364e65656ab107e465325d2/637adca2e1a09547acd85968_Y_44LwHNRnOEvnRExgnO1UujtZwn7zq7BCb4oxxHgpI.jpeg",
-        }}
-      />
+      <Navbar />
+      <Cart />
+      <main className="flex flex-col gap-16 relative">
+        <Hero heroapi={heroapi} />
+        <Sales1 endpoint={popularsales} ifExists />
+        <FlexContent endpoint={highlight} ifExists />
+        <Sales endpoint={toprateslaes} />
+        <FlexContent endpoint={sneaker} />
+        <Stories story={story} />
+      </main>
+      <Footer footerAPI={footerAPI} />
     </>
   );
 };
