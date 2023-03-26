7351830c75ccdd211e83e0362e653ffef0bbc097
JanWeeee <104844690+JanWeeee@users.noreply.github.com>
2023-03-26 19:02:11 +0530

    Footer

--- src/data/data.js
+++ src/data/data.js
@@ -332,30 +332,22 @@ const story = {
 
 
 const footerAPI = {
-  titles: [ {title: "About Nike"},{title: "Get Help"},{title: "Company"} ],
+  titles: [ {title: "About NiftyPerks"},{title: "Get Help"},{title: "Company"} ],
   links: [
     [
-      {link: "News"},
-      {link: "Careers"},
-      {link: "Investors"},
       {link: "Prupose"},
-      {link: "Sustainability"},
+      {link: "News"},
+      {link: "Careers"},,
     ],
     [
       {link: "Order Status"},
       {link: "Shipping & Delivery"},
       {link: "Payment Options"},
-      {link: "Gift Card Balance"},
       {link: "Contact Us"},
-      {link: "FAQ"},
-      {link: "Blog"},
     ],
     [
       {link: "Gift Cards"},
       {link: "Promotions"},
-      {link: "Find A Store"},
-      {link: "Signup"},
-      {link: "Nike Jouneral"},
       {link: "Send Us Feeback"},
     ],
   ]
