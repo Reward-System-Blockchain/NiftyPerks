9bc3274e52efe653c27f3ade6de9553ef7c8d0a2
Harsh Gupta <harsh.gupta2211@gmail.com>
2023-03-26 13:41:16 +0530

    update profile

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,21 +1,34 @@
 import React from "react";
 import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
 import { Card1 } from "./components/utils/Card1.jsx";
 import { Grid } from "@nextui-org/react";
+import { footerAPI } from "./data/data.js";
 
 const Profile = () => {
   return (
-    <div className="h-screen flex flex-col">
-      <Navbar />
-      <h1>Profile</h1>
-      <p>This is a profile page.</p>
-
-      <Grid.Container gap={2} justify="center">
-        <Grid xs={12} sm={4}>
-          <Card1 />
-        </Grid>
-      </Grid.Container>
-    </div>
+    <>
+      <div className="relative h-auto w-auto flex flex-col ">
+        <Navbar />
+        <div className="bg-theme clip-path h-[65vh] lg:h-[75vh] md:h-[65vh] sm:h-[55vh] w-auto absolute top-0 left-0 right-0 opacity-100 z-10 "></div>
+        <div className=" py-14 ">
+          <div className="relative opacity-100 z-20 grid items-center justify-items-center ">
+            <div className="grid items-center justify-items-center mt-12 md:mt-24 ">
+              {/* <h1>Profile</h1>
+          <p>This is a profile page.</p> */}
+            </div>
+            <div className="grid items-center justify-items-center mt-12 md:mt-24 ">
+              <Grid.Container gap={2} justify="center">
+                <Grid>
+                  <Card1 />
+                </Grid>
+              </Grid.Container>
+            </div>
+          </div>
+        </div>
+        <Footer footerAPI={footerAPI} />
+      </div>
+    </>
   );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,11 +30,8 @@ const Navbar = () => {
   };
   const onUserToggle = () => {
     location.href = "/profile";
-   console.log(
-      "User Profile Page"
-   )
+    console.log("User Profile Page");
   };
-
 
   const onNavScroll = () => {
     if (window.scrollY > 30) {
@@ -55,7 +52,7 @@ const Navbar = () => {
       <header
         className={
           !navState
-            ? "absolute top-7 left-0 right-0 opacity-100 z-50"
+            ? "absolute top-4 left-0 right-0 opacity-100 z-50"
             : "fixed top-0 left-0 right-0 h-[9vh] flex items-center justify-center opacity-100 z-[200] blur-effect-theme"
         }
       >
@@ -69,17 +66,17 @@ const Navbar = () => {
           </div>
           <ul className="flex items-center justify-center gap-2">
             <li className="grid items-center">
-              <button 
-               type="button"
-               onClick={onUserToggle}
-               className="border-none outline-none active:scale-110 transition-all duration-300 relative"
-             >
-              <UserIcon
-                className={`icon-style ${
-                  navState && "text-slate-900 transition-all duration-300"
-                }`}
-              />
-              {/* <div
+              <button
+                type="button"
+                onClick={onUserToggle}
+                className="border-none outline-none active:scale-110 transition-all duration-300 relative"
+              >
+                <UserIcon
+                  className={`icon-style ${
+                    navState && "text-slate-900 transition-all duration-300"
+                  }`}
+                />
+                {/* <div
                   className={`absolute top-4 right-0 shadow w-4 h-4 text-[0.65rem] leading-tight font-medium rounded-full flex items-center justify-center cursor-pointer hover:scale-110 transition-all duration-300 ${
                     navState
                       ? "bg-slate-900 text-slate-100 shadow-slate-900"
