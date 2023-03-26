72e18f583afeec6c15c865b58236352bdf0bec1f
RonikParashar <ronikparashar123@gmail.com>
2023-03-26 15:55:49 +0530

    profile page card

---
+++ src/Card2.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="card">
+      <svg
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        viewBox="0 0 784.37 1277.39"
+        clipRule="evenodd"
+        fillRule="evenodd"
+        imageRendering="optimizeQuality"
+        textRendering="geometricPrecision"
+        shapeRendering="geometricPrecision"
+        version="1.1"
+        height="100%"
+        width="100%"
+        xmlSpace="preserve"
+        className="img"
+      >
+        <g id="Layer_x0020_1">
+          <metadata id="CorelCorpID_0Corel-Layer"></metadata>
+          <g id="_1421394342400">
+            <g>
+              <polygon
+                points="392.07,0 383.5,29.11 383.5,873.74 392.07,882.29 784.13,650.54"
+                fillRule="nonzero"
+                fill="#343434"
+              ></polygon>
+              <polygon
+                points="392.07,0 -0,650.54 392.07,882.29 392.07,472.33"
+                fillRule="nonzero"
+                fill="#8C8C8C"
+              ></polygon>
+              <polygon
+                points="392.07,956.52 387.24,962.41 387.24,1263.28 392.07,1277.38 784.37,724.89"
+                fillRule="nonzero"
+                fill="#3C3C3B"
+              ></polygon>
+              <polygon
+                points="392.07,1277.38 392.07,956.52 -0,724.89"
+                fillRule="nonzero"
+                fill="#8C8C8C"
+              ></polygon>
+              <polygon
+                points="392.07,882.29 784.13,650.54 392.07,472.33"
+                fillRule="nonzero"
+                fill="#141414"
+              ></polygon>
+              <polygon
+                points="0,650.54 392.07,882.29 392.07,472.33"
+                fillRule="nonzero"
+                fill="#393939"
+              ></polygon>
+            </g>
+          </g>
+        </g>
+      </svg>
+      <div className="textBox">
+        <p className="text head">Ethereum</p>
+        <span>Cryptocurrency</span>
+        <p className="text price">1.654,34€</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
