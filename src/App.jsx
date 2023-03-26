14c3413003f6af5ef7d577cc5c43aa3059f1fac8
Harsh Gupta <90423812+hrsh22@users.noreply.github.com>
2023-03-26 16:36:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -51,36 +51,17 @@ const App = () => {
   return (
     <>
       {" "}
-      <WagmiConfig client={wagmiClient}>
-        <Navbar />
-        <Cart />
-        <main className="flex flex-col gap-16 relative">
-          <Hero heroapi={heroapi} />
-          <Sales1 endpoint={popularsales} ifExists />
-          <FlexContent endpoint={highlight} ifExists />
-          <Sales endpoint={toprateslaes} />
-          <FlexContent endpoint={sneaker} />
-          {/* <Stories story={story} /> */}
-        </main>
-        <Footer footerAPI={footerAPI} />
-      </WagmiConfig>
-      <Web3Modal
-        projectId={projectId}
-        ethereumClient={ethereumClient}
-        themeVariables={
-          {
-            // '--w3m-font-family': 'ui-sans-serif,system-ui',
-            // "--w3m-accent-color": "#334155",
-          }
-        }
-        themeMode="light"
-        enableNetworkView="true"
-        chainImages={{
-          5: "https://assets-global.website-files.com/6364e65656ab107e465325d2/637aee14aa9d9f521437ec16_hYC2y965v3QD7fEoVvutzGbJzVGLSOk6RZPwEQWcA_E.jpeg",
-          80001:
-            "https://assets-global.website-files.com/6364e65656ab107e465325d2/637adca2e1a09547acd85968_Y_44LwHNRnOEvnRExgnO1UujtZwn7zq7BCb4oxxHgpI.jpeg",
-        }}
-      />
+      <Navbar />
+      <Cart />
+      <main className="flex flex-col gap-16 relative">
+        <Hero heroapi={heroapi} />
+        <Sales1 endpoint={popularsales} ifExists />
+        <FlexContent endpoint={highlight} ifExists />
+        <Sales endpoint={toprateslaes} />
+        <FlexContent endpoint={sneaker} />
+        <Stories story={story} />
+      </main>
+      <Footer footerAPI={footerAPI} />
     </>
   );
 };
